2b471e5d8e2e252a23c98ca4da178c8bb4300e50
volard <kirillzhilenkov@gmail.com>
2025-11-08 02:53:28 +0800

    refactor: simplify user model

--- src/main/java/com/b216/umrs/features/auth/model/SecurityUser.java
+++ src/main/java/com/b216/umrs/features/auth/model/SecurityUser.java
@@ -1,32 +1,27 @@
 package com.b216.umrs.features.auth.model;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
-
+import com.b216.umrs.features.auth.domain.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.b216.umrs.features.auth.domain.User;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class SecurityUser implements UserDetails {
-
-    private User user;
-
-    public SecurityUser(User user) {
-        this.user = user;
-    }
+public record SecurityUser(User user) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         // Convert user's roles and scopes to List of SimpleGrantedAuthority
-        return java.util.stream.Stream.concat(
+        return Stream.concat(
                 user.getRoles().stream()
-                        .map(role -> new org.springframework.security.core.authority.SimpleGrantedAuthority(
-                                "ROLE_" + role.getName())),
+                    .map(role -> new SimpleGrantedAuthority(
+                        "ROLE_" + role.getName())),
                 user.getScopes().stream()
-                        .map(scope -> new org.springframework.security.core.authority.SimpleGrantedAuthority(
-                                "SCOPE_" + scope.getName())))
-                .collect(Collectors.toList());
+                    .map(scope -> new SimpleGrantedAuthority(
+                        "SCOPE_" + scope.getName())))
+            .collect(Collectors.toList());
     }
 
     @Override
