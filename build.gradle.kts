369161ef30e42f9c23138a2a686d0105217112af
volard <kirillzhilenkov@gmail.com>
2026-03-17 01:15:19 +0800

    [Feature] Update Java to 25 Fixes #8

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "4.0.3"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.asciidoctor.jvm.convert") version "3.3.2"
+    id("org.asciidoctor.jvm.convert") version "4.0.5"
 }
 
 group = "com.b216"
@@ -11,7 +11,7 @@ description = "Backend service"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
