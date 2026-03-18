00478b72e60a27e129ff80a3278811e5e14a299f
volard <kirillzhilenkov@gmail.com>
2026-03-18 20:20:08 +0800

    fix: lower security policies for movements form submission endpoint

--- src/main/java/com/b216/umrs/features/forms/movements/MovementsFormController.java
+++ src/main/java/com/b216/umrs/features/forms/movements/MovementsFormController.java
@@ -14,7 +14,10 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/public/forms/movements")
+@RequestMapping({
+    "/api/v1/public/forms/movements",
+    "/v1/public/forms/movements"
+})
 public class MovementsFormController {
 
     private final MovementsFormService movementsFormService;
